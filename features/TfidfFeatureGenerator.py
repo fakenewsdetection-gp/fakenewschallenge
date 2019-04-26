fe0529c58294cf2976304fc8f5c751d729dd8875
anas.harby <anas.harby@gmail.com>
2019-04-27 00:27:42 +0200

    Simplify calculations of cosine similarity between tf-idf vectors

--- features/TfidfFeatureGenerator.py
+++ features/TfidfFeatureGenerator.py
@@ -19,7 +19,7 @@ class TfidfFeatureGenerator(FeatureGenerator):
             return res
 
         n_train = df[~df['target'].isnull()].shape[0]
-        n_features = 5000
+        n_features = 200
 
         vectorizer_headlines = TfidfVectorizer(ngram_range=(1, 3), max_features=n_features)
         tfidf_headlines = vectorizer_headlines.fit_transform(
@@ -33,21 +33,17 @@ class TfidfFeatureGenerator(FeatureGenerator):
         tfidf_bodies_train = tfidf_bodies[:n_train, :]
         tfidf_bodies_test  = tfidf_bodies[n_train:, :]
 
+        tfidf_cos_sim = np.zeros((df.shape[0], 1))
+        for i in range(df.shape[0]):
+            tfidf_cos_sim[i] = cosine_similarity(
+                tfidf_headlines[i].reshape(1, -1), tfidf_bodies[i].reshape(1, -1))
+        tfidf_cos_sim_train = tfidf_cos_sim[:n_train]
+        tfidf_cos_sim_test  = tfidf_cos_sim[n_train:]
+
         self._dump(tfidf_headlines_train, 'train.headline.tfidf.pkl')
         self._dump(tfidf_headlines_test, 'test.headline.tfidf.pkl')
         self._dump(tfidf_bodies_train, 'train.body.tfidf.pkl')
         self._dump(tfidf_bodies_test, 'test.body.tfidf.pkl')
-
-        for f in [tfidf_headlines_train, tfidf_headlines_test, tfidf_bodies_train, tfidf_bodies_test]:
-            del f
-        gc.collect()
-
-        tfidf_cos_sim = np.diagonal(cosine_similarity(
-            tfidf_headlines, tfidf_bodies)).reshape(-1, 1)
-        print(tfidf_cos_sim.shape)
-        tfidf_cos_sim_train = tfidf_cos_sim[:n_train]
-        tfidf_cos_sim_test  = tfidf_cos_sim[n_train:]
-
         self._dump(tfidf_cos_sim_train, 'train.sim.tfidf.pkl')
         self._dump(tfidf_cos_sim_test, 'test.sim.tfidf.pkl')
 
@@ -60,4 +56,4 @@ class TfidfFeatureGenerator(FeatureGenerator):
         for f in files:
             with open(f, 'rb') as infile:
                 res.append(pickle.load(infile))
-        return np.array(res)
+        return res
