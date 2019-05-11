24b3fcc9bb8a17080b7c8e06d51ce3c4a44447e3
Mohamed A. Tolba <tolba96mohamed@gmail.com>
2019-05-11 13:54:43 +0200

    Modify generate_features module

--- generate_features.py
+++ generate_features.py
@@ -1,63 +1,25 @@
 import pandas as pd
 import pickle
+from dataset import Dataset
 from features.tf_idf_feature_generator import TfidfFeatureGenerator
-from features.sentiment_feature_generator import SentimentFeatureGenerator
 
 
-def process():
-	if read_flag:
-		# Read training set
-		body_train = pd.read_csv('fnc-1/train_bodies.csv', encoding='utf-8')
-		stances_train = pd.read_csv('fnc-1/train_stances.csv', encoding='utf-8')
+# Set file names
+file_train_instances = "fnc-1/train_stances.csv"
+file_train_bodies = "fnc-1/train_bodies.csv"
+file_test_instances = "fnc-1/competition_test_stances.csv"
+file_test_bodies = "fnc-1/competition_test_bodies.csv"
 
-		train = pd.merge(stances_train, body_train, how='left', on='Body ID')
-		targets = ['agree', 'disagree', 'discuss', 'unrelated']
-		targets_dict = dict(zip(targets, range(len(targets))))
-		train['target'] = train['Stance'].map(lambda x: targets_dict[x])
+# Load data sets
+raw_train = Dataset(file_train_instances, file_train_bodies)
+raw_test = Dataset(file_test_instances, file_test_bodies)
 
-		if N != -1:
-			train = train[:N]
+max_num_words = 5000
 
-		data = train
-		# Read testing set, concatenate both training and testing set in data.
-		if test_flag:
-			body_test = pd.read_csv('fnc-1/test_bodies.csv', encoding='utf-8')
-			headline_test = pd.read_csv('fnc-1/test_stances_unlabeled.csv', encoding='utf-8')
-			test = pd.merge(headline_test, body_test, how='left', on='Body ID')
-
-			if N != -1:
-				test = test[:N]
-
-			data = pd.concat((train, test))
-
-			train = data[~data['target'].isnull()]
-			test = data[data['target'].isnull()]
-
-			print('Training set shape: ', train.shape)
-			print('Testing set shape: ', test.shape)
-
-		# Generate unigrams
-		print('Generating unigrams...')
-		data['Headline_unigram'] = data['Headline'].map(lambda x: preprocess_data(x))
-		data['articleBody_unigram'] = data['articleBody'].map(lambda x: preprocess_data(x))
-
-		# Write to file
-		data.to_pickle('data.pkl')
-
-	else:
-		data = pd.read_pickle('data.pkl')
-		print('Loaded data from data.pkl')
-		print('Data shape: ', data.shape)
-
-	for g in generators:
-		g.process(data)
-
-	for g in generators:
-		g.read('train')
-
-	for g in generators:
-		g.read('test')
-
-
-if __name__ == '__main__':
-	process()
+# Process data sets
+tfidfFeatureGenerator = TfidfFeatureGenerator()
+train_data, bow_vectorizer, tfreq_vectorizer, tfidf_vectorizer = \
+    tfidfFeatureGenerator.process_train(raw_train, raw_test, max_num_words=max_num_words)
+feature_size = len(train_data['features'][0])
+test_data = tfidfFeatureGenerator.process_test(raw_test, bow_vectorizer,
+                                                tfreq_vectorizer, tfidf_vectorizer)
--- main.py
+++ main.py
@@ -17,10 +17,6 @@ random.seed(42)
 mode = input('mode (load / train)? ')
 
 # Set file names
-file_train_instances = "fnc-1/train_stances.csv"
-file_train_bodies = "fnc-1/train_bodies.csv"
-file_test_instances = "fnc-1/competition_test_stances.csv"
-file_test_bodies = "fnc-1/competition_test_bodies.csv"
 file_predictions = "predictions_test.csv"
 models_dir = "models"
 mlp_model_file = "mlp.hdf5"
@@ -34,18 +30,11 @@ learning_rate = 0.005
 batch_size = 500
 epochs = 5
 
-# Load data sets
-raw_train = Dataset(file_train_instances, file_train_bodies)
-raw_test = Dataset(file_test_instances, file_test_bodies)
-n_train = len(raw_train.instances)
-
 # Process data sets
 tfidfFeatureGenerator = TfidfFeatureGenerator()
-train_data, bow_vectorizer, tfreq_vectorizer, tfidf_vectorizer = \
-    tfidfFeatureGenerator.process_train(raw_train, raw_test, max_num_words=max_num_words)
+train_data = tfidfFeatureGenerator.load()
 feature_size = len(train_data['features'][0])
-test_data = tfidfFeatureGenerator.process_test(raw_test, bow_vectorizer,
-                                                tfreq_vectorizer, tfidf_vectorizer)
+test_data = tfidfFeatureGenerator.load(header='test')
 
 # Check if models directory doesn't exist
 if not os.path.isdir(models_dir):
